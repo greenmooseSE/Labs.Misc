2e45fc287fed769c6e62a2eff94bad044dcb8e68
GreenMoose <juggernaut78@gmail.com>
2023-02-07 21:22:04 +0100

    Try add naming convention for tests and async [try-newProj]

--- NamingStylesLib/Class1.cs
+++ NamingStylesLib/Class1.cs
@@ -1,15 +1,45 @@
-﻿namespace NamingStylesLib;
+﻿using JetBrains.Annotations;
+using NUnit.Framework;
+
+namespace NamingStylesLib;
 public class MyClass
 {
     private int _field;
+    private static int _staticField;
+    [UsedImplicitly]
     public void StandardMethod()
     {
         _field += 1;
     }
 
+    [UsedImplicitly]
+    public static void StaticMethod()
+    {
+        _staticField += 1;
+    }
+
+    [UsedImplicitly]
     public async Task AsyncMethod()
     {
         _field += 1;
+        await Task.CompletedTask;
+    }
+}
+
+
+[TestFixture]
+public class SomeNUnitFixture
+{
+    [Test]
+    public void StandardTest()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public async Task AsyncTest()
+    {
+        Assert.Pass();
         await Task.CompletedTask;
     }
 }
